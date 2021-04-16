3516a335491c351373057e0bf4c33d7d94a5a72b
AbdullahSB2020 <abdullah.salim.pt@gmail.com>
2021-04-16 23:13:22 +0300

    Some code has been added to user Model

--- user.js
+++ user.js
@@ -1,1 +1,13 @@
-console.log("This is the user model");
+console.log("This is the user model")
+
+console.log("I have added new code now")
+
+let updated = true ;
+
+if(updated){
+	console.log("We have added some new featues")
+}
+else {
+	console.log("There is nothing to view")
+}
+
