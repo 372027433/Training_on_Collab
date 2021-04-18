fff2a71f50a1a01621b0d2fc970a07ff888ff8de
AbdullahSB2020 <abdullah.salim.pt@gmail.com>
2021-04-19 02:58:47 +0300

    posts contain married status

--- posts.js
+++ posts.js
@@ -1,3 +1,11 @@
 console.log("Post for new comers")
 
-console.log("we have these posts for beginers")
+console.log("we have these posts for beginers");
+
+var haveWife = false ;
+if(haveWife) {
+	console.log("he is married")
+} else {
+	console.log("He is not fucking married")
+}
+
